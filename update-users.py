ce3f33e48ff3e18c297254a051ccc48e2d55a124
reinvantveer <rein@vantveer.me>
2024-10-02 16:05:28 +0200

    disable users not in the master list

--- update-users.py
+++ update-users.py
@@ -2,26 +2,37 @@ import argparse
 import csv
 from argparse import Namespace
 
+from loguru import logger
 from nc_py_api import Nextcloud
 
 
 def main(args: Namespace):
     records: list[dict[str, str]] = []
+    user_names: list[str] = []
 
     with open(args.file) as f:
         # Parse the CSV file
         reader = csv.DictReader(f)
         for row in reader:
             records.append(row)
+            user_names.append(row['gebruikersnaam'])
 
     # Read out the users in Nextcloud
     nc = Nextcloud(nextcloud_url=args.server, username=args.username, password=args.password)
     users = []
     for user_id in nc.users.get_list():
-        users.append(nc.users.get_user(user_id))
+        user = nc.users.get_user(user_id)
+        users.append(user)
+
+        if user.user_id not in user_names:
+            logger.warning(f'Disabling user {user.user_id} not found in the master user list')
+            if not args.dry_run:
+                nc.users.disable(user.user_id)
+            else:
+                logger.info(f'Dry-run: Would have disabled user {user.user_id}')
 
     for record in records:
-        print(f"Updating user: {record['username']}")
+        logger.info(f'Updating user: {record['username']}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Update users in the Nextcloud database')
@@ -39,6 +50,9 @@ if __name__ == '__main__':
     server_help = 'The URL of the Nextcloud server'
     server_default = 'https://https://nx52347.your-storageshare.de/'
     parser.add_argument('-s', 'server', type=str, help=server_help, default=server_default)
+
+    dry_run_help = 'Do not actually update the users'
+    parser.add_argument('--dry-run', action='store_true', help=dry_run_help, default=True)
 
     args = parser.parse_args()
     main(args)
