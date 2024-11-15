1639971f08970c98d9b8bcbacaf645b357b02beb
reinvantveer <rein@vantveer.me>
2024-11-15 12:45:34 +0100

    rename symbol

--- tests/test_master_userlist.py
+++ tests/test_master_userlist.py
@@ -1,13 +1,11 @@
-import os
-
 from nextcloud.master_userlist import load_users_from_spreadsheet
 from nextcloud.nextcloud_server import Nextcloud
 
 
 def test_get_master_userlist():
     nc = Nextcloud()
-    userlist = load_users_from_spreadsheet(nc)
+    user_list = load_users_from_spreadsheet(nc)
 
     # Make sure that all user ids are email addresses
-    for user_id in userlist:
+    for user_id in user_list:
         assert '@' in user_id
