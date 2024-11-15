eb9d678ac002abe2c6f84d360b1680b0a1f07c24
reinvantveer <rein@vantveer.me>
2024-11-15 12:19:21 +0100

    add test to validate loading users

--- tests/test_master_userlist.py
+++ tests/test_master_userlist.py
@@ -1,9 +1,13 @@
 import os
 
-from nextcloud.master_userlist import load_users
+from nextcloud.master_userlist import load_users_from_spreadsheet
 from nextcloud.nextcloud_server import Nextcloud
 
 
 def test_get_master_userlist():
-    nc = Nextcloud(username='Admin', password=os.environ['password'], server="https://nextcloud.example.com")
-    userlist = load_users("pyproject.toml")
+    nc = Nextcloud()
+    userlist = load_users_from_spreadsheet(nc)
+
+    # Make sure that all user ids are email addresses
+    for user_id in userlist:
+        assert '@' in user_id
