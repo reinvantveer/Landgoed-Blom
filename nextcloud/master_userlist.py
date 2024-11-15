3601945a5c105e05f3043790cfe59adcfd9a4408
reinvantveer <rein@vantveer.me>
2024-11-15 12:18:16 +0100

    use pandas to load user list from spreadsheet

--- nextcloud/master_userlist.py
+++ nextcloud/master_userlist.py
@@ -1,13 +1,22 @@
+import pandas
 import requests
 
 from nextcloud.nextcloud_server import Nextcloud
 
 
-def load_users(nextcloud: Nextcloud, file_id: str) -> list[dict[str, str]]:
-    # Use the ocs API to get the file by id
+def load_users_from_spreadsheet(nextcloud: Nextcloud) -> set[str]:
+    # Use the WebDAV API to get the file by id
+
     resp = requests.get(
-        url=f'{nextcloud.server}/ocs/v1.php/cloud/files/{file_id}',
+        url=f'{nextcloud.server}/remote.php/dav/files/{nextcloud.username}/{nextcloud.file_path}',
         auth=(nextcloud.username, nextcloud.password),
         headers={'OCS-APIRequest': 'true'}
     )
     resp.raise_for_status()
+
+    # Open the Excel file and read the users
+    df = pandas.read_excel(resp.content)
+    # We always use mail addresses as the user id
+    mail_addresses = df['Mail'].filter(regex='@', axis=0)
+
+    return set(mail_addresses)
