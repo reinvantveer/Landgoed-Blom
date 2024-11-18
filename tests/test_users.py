08cd21efb540444855ffe6e2057cc46fffc04b2c
reinvantveer <rein@vantveer.me>
2024-11-18 12:37:31 +0100

    add and clean up test user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,6 @@
-from nextcloud.nextcloud_server import Nextcloud
-from nextcloud.users import get_user_ids, create_user
+from nextcloud.auth import generate_password
+from nextcloud.nextcloud_server import Nextcloud, NextcloudUser
+from nextcloud.users import get_user_ids, create_user, delete_user
 
 
 def test_get_user_ids():
@@ -13,9 +14,14 @@ def test_get_user_ids():
 # Integration tests
 def test_add_user():
     nc = Nextcloud()
-    user = {
-        'userid': 'pioniersgroep@buitenhuisblom.nl',
-        'password': 'test_password',
-        'displayName': 'Test User'
-    }
+    user = 'pioniersgroep@buitenhuisblom.nl'
+    user = NextcloudUser(userid=user, email=user, password=generate_password(), displayName='Test User')
     create_user(nc, user)
+    user_ids = get_user_ids(nc)
+    assert user.userid in user_ids
+
+    # Clean up
+    delete_user(nc, user)
+    user_ids = get_user_ids(nc)
+    assert user.userid not in user_ids
+
