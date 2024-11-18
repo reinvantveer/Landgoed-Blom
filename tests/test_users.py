c0e4a973762fc4d69ca4351493a1aef2dce8150c
reinvantveer <rein@vantveer.me>
2024-11-18 12:18:37 +0100

    add integration test that actually creates a user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,5 @@
 from nextcloud.nextcloud_server import Nextcloud
-from nextcloud.users import get_user_ids
+from nextcloud.users import get_user_ids, create_user
 
 
 def test_get_user_ids():
@@ -9,3 +9,13 @@ def test_get_user_ids():
     # Validate that all user ids are email addresses
     for user_id in user_ids:
         assert '@' in user_id
+
+# Integration tests
+def test_add_user():
+    nc = Nextcloud()
+    user = {
+        'userid': 'pioniersgroep@buitenhuisblom.nl',
+        'password': 'test_password',
+        'displayName': 'Test User'
+    }
+    create_user(nc, user)
